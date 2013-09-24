5218dc7d6867a57a811f99ac7c0eadbb3da2c843
Serban Carlogea <sherban.carlogea@gmail.com>
2013-09-24 23:30:31 +0200

    its too late, we will see tomorrow

--- browser.py
+++ browser.py
@@ -3,7 +3,10 @@ __email__  = 'sherban.carlogea@gmail.com'
 
 import logging
 
+
 class Browser(object):
-    
-    def __init__(self, proxy=None, use_random_proxy=False, user_agent=None, use_random_user_agent=False, logger=logging):
-        pass
+
+    def __init__(self, proxy=None, user_agent=None, logger=logging):
+        self.__proxy = proxy
+        self.__user_agent = user_agent
+        self.__logger = logger
--- crawler.py
+++ crawler.py
@@ -5,23 +5,15 @@ from running import Running
 from browser import Browser
 import logging
 
+
 class Crawler(Running):
-    
+
+    __browser = None
+
     def __init__(self, browser=None, logger=logging):
         Running.__init__(self)
-        pass
-
-    def set_browser(self, browser=None):
         if isinstance(browser, Browser):
             self.__browser = browser
         else:
-            self.__browser = Browser()
-
-    def get_browser(self):
-        return self.__browser
-
-    def set_logger(self, logger=None):
+            self.__browser = Browser(logger=self.__logger)
         self.__logger = logger
-
-    def get_logger(self):
-        return self.__logger
