a93f626fd8c8fdeef42c1fc5f6be8a3f2299ee84
Sherban Carlogea <sherban.carlogea@gmail.com>
2013-09-24 20:15:07 +0000

    added basic setter and getter for crawler

--- crawler.py
+++ crawler.py
@@ -2,7 +2,26 @@ __author__ = "Serban Carlogea"
 __email__  = "sherban.carlogea@gmail.com"
 
 from running import Running
-
+from browser import Browser
+import logging
 
 class Crawler(Running):
-    pass
+    
+    def __init__(self, browser=None, logger=logging):
+        Running.__init__(self)
+        pass
+
+    def set_browser(self, browser=None):
+        if isinstance(browser, Browser):
+            self.__browser = browser
+        else:
+            self.__browser = Browser()
+
+    def get_browser(self):
+        return self.__browser
+
+    def set_logger(self, logger=None):
+        self.__logger = logger
+
+    def get_logger(self):
+        return self.__logger
