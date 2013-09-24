ffeed3fa322708675b0031d992e8251c1b706bb1
Sherban Carlogea <sherban.carlogea@gmail.com>
2013-09-24 20:14:41 +0000

    added init skeleton for basic browser

--- browser.py
+++ browser.py
@@ -1,6 +1,9 @@
 __author__ = 'Serban Carlogea'
 __email__  = 'sherban.carlogea@gmail.com'
 
+import logging
 
 class Browser(object):
-    pass
+    
+    def __init__(self, proxy=None, use_random_proxy=False, user_agent=None, use_random_user_agent=False, logger=logging):
+        pass
